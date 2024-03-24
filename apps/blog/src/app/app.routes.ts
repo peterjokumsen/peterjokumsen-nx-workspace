33198b8f905848ce6a810498f6deee752cd119ab
Peter Jokumsen <pjokumsen@gmail.com>
2024-03-24 18:50:08 +0200

    feat(blog): add landing page

--- apps/blog/src/app/app.routes.ts
+++ apps/blog/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/landing.component').then((m) => m.LandingComponent),
+  },
+];
