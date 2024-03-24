d91c83b42a0441d6f529cdcd8623d580acf520ad
Peter Jokumsen <pjokumsen@gmail.com>
2024-03-24 13:56:06 +0000

    chore(e2e): adjust playwright config

--- apps/blog-e2e/playwright.config.ts
+++ apps/blog-e2e/playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   webServer: {
     command: 'npx nx serve blog',
     url: 'http://localhost:4200',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !process.env['CI'],
     cwd: workspaceRoot,
   },
   projects: [
