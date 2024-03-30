8b54c9926e2766733caf7e4283772a353dc3eabe
Peter Jokumsen <pjokumsen@gmail.com>
2024-03-30 20:32:18 +0200

    feat(blog): initial change for about-me content

--- apps/blog/src/app/pages/about-me.component.ts
+++ apps/blog/src/app/pages/about-me.component.ts
@@ -4,7 +4,13 @@ import { CommonModule } from '@angular/common';
 @Component({
   standalone: true,
   imports: [CommonModule],
-  template: `<p>about-me works!</p>`,
+  template: `
+  <div class="flex flex-col items-center justify-center h-screen">
+    <code class="bg-stone-100 border-2 border-stone-400 rounded p-12 items-center">
+      // TODO: Add content here
+    </code>
+  </div>
+  `,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
