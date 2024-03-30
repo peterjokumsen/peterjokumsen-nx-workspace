bf73adc70e32857205d435cd19f41a6c01722e4c
Peter Jokumsen <pjokumsen@gmail.com>
2024-03-30 19:54:02 +0200

    feat(blog): add basic layout with content

--- apps/blog/src/app/app.component.ts
+++ apps/blog/src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
 @Component({
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLinkActive, RouterLink, FaIconComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styles: '',
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly navElements: Array<{ route: string; title: string }> = [
+    { route: '/home', title: 'Home' },
+  ];
+  codeIcon = faCode;
+}
