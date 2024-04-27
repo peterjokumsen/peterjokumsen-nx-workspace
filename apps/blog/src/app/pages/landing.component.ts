83e18f8c0e91d08149bdd5265d2f5f9cd076dab6
Peter Jokumsen <pjokumsen@gmail.com>
2024-04-27 14:31:01 +0200

    feat(blog): add background to `home`

--- apps/blog/src/app/pages/landing.component.ts
+++ apps/blog/src/app/pages/landing.component.ts
@@ -6,18 +6,22 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <div class="flex min-h-screen flex-col items-center justify-center">
-      <div
-        class="items-center rounded border-2 border-stone-400 bg-stone-100 p-12"
-      >
-        <h1 class="mb-16 justify-start text-3xl">👋 Hi there!</h1>
-        <p>Welcome to my blog!</p>
-        <p>This is a work in progress.</p>
-        <p>Please bear with me, while I practice my skills.</p>
+    <div class="background">
+      <div class="flex min-h-screen flex-col items-start">
+        <div
+          class="m-5 items-center rounded border-2 border-stone-400 bg-stone-100 p-12 opacity-70"
+        >
+          <div class="font-bold opacity-100">
+            <h1 class="mb-16 justify-start text-3xl">👋 Hi there!</h1>
+            <p>Welcome to my blog!</p>
+            <p>This is a work in progress.</p>
+            <p>Please bear with me, while I practice my skills.</p>
+          </div>
+        </div>
       </div>
     </div>
   `,
-  styles: ``,
+  styleUrl: 'landing.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LandingComponent {}
