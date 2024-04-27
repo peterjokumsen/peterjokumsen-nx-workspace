3d2cf1c2bb8409be458d130e61a2f7d1a9e79571
Peter Jokumsen <pjokumsen@gmail.com>
2024-04-28 00:20:25 +0200

    feat(blog): use article component for about-me

--- apps/blog/src/app/pages/about-me.component.ts
+++ apps/blog/src/app/pages/about-me.component.ts
@@ -1,208 +1,39 @@
-import { ActivatedRoute, Router } from '@angular/router';
-import {
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  OnDestroy,
-  OnInit,
-  ViewChild,
-  inject,
-  signal,
-} from '@angular/core';
-import { Subject, firstValueFrom, map } from 'rxjs';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { ArticleComponent } from '@peterjokumsen/ui-elements';
 import { CommonModule } from '@angular/common';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { faMapLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, FaIconComponent],
-  template: `
-    <ng-template #headersTemplate>
-      <div
-        class="table-of-contents sticky top-0 flex max-h-screen flex-col justify-around gap-2 overflow-y-auto p-2"
-      >
-        @for (header of headers(); track header.title) {
-          <a
-            class="nav-button primary-block alt-text rounded border px-4 py-2 font-bold"
-            href="#"
-            (click)="selectHeader(header.id)"
-          >
-            {{ header.title }}
-          </a>
-        }
-      </div>
-    </ng-template>
-    <div class="content grid grid-cols-5">
-      <div
-        #content
-        class="col-span-5 md:col-span-3 md:col-start-1 lg:col-span-2 lg:col-start-2"
-      >
-        <div class="sticky top-1 col-span-5 md:hidden">
-          <div class="control flex justify-end">
-            <button
-              class="nav-button primary-block rounded p-2 px-4"
-              [ngClass]="{ active: tocExpanded }"
-              (click)="showOrHideToc()"
-              aria-label="Show or hide page navigation"
-            >
-              <fa-icon [icon]="expandIcon"></fa-icon>
-            </button>
-          </div>
-          <div #toc class="show-hide">
-            <div class="primary-block mt-2 rounded-2xl border-2 p-4">
-              <ng-container *ngTemplateOutlet="headersTemplate"></ng-container>
-            </div>
-          </div>
-        </div>
-        <div
-          class="flex h-screen flex-col items-center justify-center gap-6 p-6"
-        >
-          <h1 class="text-4xl font-bold">Who am I?</h1>
-          <p class="text-lg">
-            I am a software engineer who loves to build things. I have been
-            working in the software industry for over 10 years.
-          </p>
-          <p class="text-lg">
-            I am passionate about learning new technologies and solving problems
-            in interesting ways.
-          </p>
-        </div>
-        <div
-          class="flex h-screen flex-col items-center justify-center gap-6 p-6"
-        >
-          <h1 class="text-2xl font-bold">What is the purpose of this blog?</h1>
-          <p class="text-lg">
-            The purpose of this blog is to share my knowledge and experiences
-            with others. I hope to help others learn new things and grow as
-            software engineers.
-          </p>
-        </div>
-        <div
-          class="flex h-screen flex-col items-center justify-center gap-6 p-6"
-        >
-          <h1 class="text-2xl font-bold">What will you find on this blog?</h1>
-          <p class="text-lg">
-            On this blog, you will find articles on various topics related to
-            software engineering. I will cover topics such as programming
-            languages, frameworks, tools, best practices, workarounds that I've
-            found and some completely unrelated topics too.
-          </p>
-        </div>
-      </div>
-      <div
-        class="col-span-2 col-start-4 hidden md:block lg:col-span-1 lg:col-start-5"
-      >
-        <ng-container *ngTemplateOutlet="headersTemplate"></ng-container>
-      </div>
-    </div>
-  `,
+  imports: [CommonModule, FaIconComponent, ArticleComponent],
+  template: ` <pj-ui-article [sections]="content"></pj-ui-article> `,
   styleUrl: 'about-me.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutMeComponent implements AfterViewInit, OnDestroy, OnInit {
-  private _router = inject(Router);
-  private _route = inject(ActivatedRoute);
-
-  private _selectedHeader$ = new Subject<string>();
-  headers = signal<Array<{ id: string; element: HTMLElement; title: string }>>(
-    [],
-  );
-  expandIcon = faMapLocationDot;
-  tocExpanded = false;
-
-  @ViewChild('content') content!: ElementRef;
-  @ViewChild('toc') toc!: ElementRef;
-
-  async ngOnInit() {
-    const selectedHeader = await firstValueFrom(
-      this._route.queryParams.pipe(
-        map((qp) => qp['header'] as string | undefined),
-      ),
-    );
-    if (selectedHeader) {
-      this._selectedHeader$.next(selectedHeader);
-    }
-  }
-
-  ngOnDestroy() {
-    this._selectedHeader$.complete();
-  }
-
-  ngAfterViewInit(): void {
-    const headers = Array.from(
-      this.content.nativeElement.querySelectorAll('h1, h2, h3, h4, h5, h6'),
-    )
-      .map((el) => {
-        const header = el as HTMLElement;
-        const id = header.innerText
-          ?.toLowerCase()
-          .split(' ')
-          .map((word) => word.replace(/[^a-zA-Z0-9-]/g, ''))
-          .join('-');
-        return {
-          id,
-          element: header,
-          title: header.innerText,
-        };
-      })
-      .filter((header) => header.id);
-
-    this.headers.update(() => [
-      { id: 'top', element: this.content.nativeElement, title: 'Top' },
-      ...headers,
-    ]);
-
-    this._selectedHeader$.subscribe((headerId) => this.navigateTo(headerId));
-  }
-
-  private navigateTo(headerId: string): void {
-    const element =
-      this.headers()?.find((header) => header.id === headerId)?.element ??
-      (this.content?.nativeElement as HTMLElement);
-    if (!element) {
-      return;
-    }
-
-    if (headerId) {
-      this._router
-        .navigate([], {
-          queryParams: { header: headerId },
-          queryParamsHandling: 'merge',
-          replaceUrl: true,
-        })
-        .then();
-    } else {
-      this._router.navigate([], { queryParams: {}, replaceUrl: true }).then();
-    }
-
-    const scrollElement = element.parentElement ?? element;
-    if (!scrollElement.scrollIntoView) return;
-    scrollElement.scrollIntoView({ behavior: 'smooth' });
-    this.showOrHideToc(true);
-  }
-
-  selectHeader(headerId: string): boolean {
-    headerId = headerId === 'top' ? '' : headerId;
-    this._selectedHeader$.next(headerId);
-    return false;
-  }
-
-  showOrHideToc(forceHide = false) {
-    const element = this.toc?.nativeElement as HTMLElement;
-    if (!element) return;
-
-    if (element.classList.contains('show')) {
-      this.tocExpanded = false;
-      element.classList.remove('show');
-      return;
-    }
-
-    if (forceHide) return;
-
-    this.tocExpanded = true;
-    element.classList.add('show');
-  }
+export class AboutMeComponent {
+  content = [
+    {
+      title: 'Who am I?',
+      content: [
+        'I am a software engineer who loves to build things.',
+        'I have been working in the software industry for over 10 years.',
+        'I am passionate about learning new technologies and solving problems in interesting ways.',
+      ],
+    },
+    {
+      title: 'What is the purpose of this blog?',
+      content: [
+        'The purpose of this blog is to share my knowledge and experiences with others.',
+        'I hope to help others learn new things and grow as software engineers.',
+      ],
+    },
+    {
+      title: 'What will you find on this blog?',
+      content: [
+        'On this blog, you will find articles on various topics related to software engineering.',
+        "I will cover topics such as programming languages, frameworks, tools, best practices, workarounds that I've found and some completely unrelated topics too.",
+      ],
+    },
+  ];
 }
