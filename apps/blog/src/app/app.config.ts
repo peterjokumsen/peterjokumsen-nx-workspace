4a3d290bef534e8c185615d5f0aa7406025d11b7
Peter Jokumsen <pjokumsen@gmail.com>
2024-05-11 23:18:11 +0200

    chore(blog): start using theme toggle in root component

--- apps/blog/src/app/app.config.ts
+++ apps/blog/src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
+import {
+  providePjBrowserProviders,
+  providePjLogger,
+} from '@peterjokumsen/ng-services';
+
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideClientHydration(), provideRouter(appRoutes)],
+  providers: [
+    provideClientHydration(),
+    provideRouter(appRoutes),
+    providePjLogger({ production: !isDevMode() }),
+    providePjBrowserProviders(),
+  ],
 };
