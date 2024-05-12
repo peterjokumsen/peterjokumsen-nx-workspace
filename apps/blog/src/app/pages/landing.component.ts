1e0ecb18c4e758ca0d509965215c56323ad035b4
Peter Jokumsen <pjokumsen@gmail.com>
2024-05-12 22:51:33 +0200

    feat(styles): update to allow opacity for backgrounds

--- apps/blog/src/app/pages/landing.component.ts
+++ apps/blog/src/app/pages/landing.component.ts
@@ -9,9 +9,9 @@ import { CommonModule } from '@angular/common';
     <div class="background">
       <div class="flex min-h-screen flex-col items-start">
         <div
-          class="m-5 items-center rounded border-2 border-stone-400 bg-stone-100 p-12 opacity-70"
+          class="main-colors m-5 items-center rounded border-2 bg-opacity-50 p-12"
         >
-          <div class="font-bold opacity-100">
+          <div class="font-bold">
             <h1 class="mb-16 justify-start text-3xl">👋 Hi there!</h1>
             <p>Welcome to my blog!</p>
             <p>This is a work in progress.</p>
