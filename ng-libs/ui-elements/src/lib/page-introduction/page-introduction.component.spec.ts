b2e19a9dacae80c0ab3994907cfab5eac5884d99
Peter Jokumsen <pjokumsen@gmail.com>
2024-05-16 23:09:44 +0200

    feat(ui-elements): implement page-introduction

--- ng-libs/ui-elements/src/lib/page-introduction/page-introduction.component.spec.ts
+++ ng-libs/ui-elements/src/lib/page-introduction/page-introduction.component.spec.ts
@@ -1,6 +1,26 @@
+import { Component, ViewChild } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { IntroductionCallToAction } from './models';
 import { PageIntroductionComponent } from './page-introduction.component';
+
+@Component({
+  template: `
+    <pj-ui-page-introduction
+      [title]="title"
+      [paragraphs]="paragraphs"
+      [actions]="actions"
+    ></pj-ui-page-introduction>
+  `,
+})
+class PageIntroductionComponentTestHostComponent {
+  title = 'Title';
+  paragraphs = ['Hello'];
+  actions: IntroductionCallToAction | IntroductionCallToAction[] | undefined;
+
+  @ViewChild(PageIntroductionComponent)
+  pageIntroductionComponent!: PageIntroductionComponent;
+}
 
 describe('PageIntroductionComponent', () => {
   let component: PageIntroductionComponent;
@@ -9,6 +29,7 @@ describe('PageIntroductionComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PageIntroductionComponent],
+      declarations: [PageIntroductionComponentTestHostComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PageIntroductionComponent);
@@ -18,5 +39,145 @@ describe('PageIntroductionComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('title', () => {
+    it('should have a default value', () => {
+      expect(component.title()).toEqual('👋 Hi there!');
+    });
+  });
+
+  describe('paragraphs', () => {
+    it('should default to empty array', () => {
+      expect(component.paragraphs()).toEqual([]);
+    });
+  });
+
+  describe('actions', () => {
+    it('should default to undefined', () => {
+      expect(component.actions()).toBeUndefined();
+    });
+  });
+
+  describe('when rendered in a host component', () => {
+    let hostFixture: ComponentFixture<PageIntroductionComponentTestHostComponent>;
+    let hostComponent: PageIntroductionComponentTestHostComponent;
+
+    beforeEach(async () => {
+      hostFixture = TestBed.createComponent(
+        PageIntroductionComponentTestHostComponent,
+      );
+      hostComponent = hostFixture.componentInstance;
+      hostFixture.detectChanges();
+      await hostFixture.whenStable();
+    });
+
+    it('should be created', () => {
+      expect(hostComponent?.pageIntroductionComponent).toBeTruthy();
+    });
+
+    describe('and title is set', () => {
+      it('should have title', () => {
+        expect(hostComponent.pageIntroductionComponent.title()).toEqual(
+          'Title',
+        );
+      });
+    });
+
+    describe('and paragraphs is set', () => {
+      beforeEach(() => {
+        hostComponent.paragraphs = ['Hello', 'World'];
+        hostFixture.detectChanges();
+      });
+
+      it('should set paragraphs', () => {
+        expect(hostComponent.pageIntroductionComponent.paragraphs()).toEqual([
+          'Hello',
+          'World',
+        ]);
+      });
+
+      it('should render content as paragraph elements', () => {
+        const paragraphs = hostFixture.nativeElement.querySelectorAll('p');
+        expect(paragraphs.length).toEqual(2);
+        expect(paragraphs[0].textContent).toEqual('Hello');
+        expect(paragraphs[1].textContent).toEqual('World');
+      });
+    });
+
+    describe('and actions', () => {
+      describe('is undefined', () => {
+        it('should not render actions', () => {
+          const actions =
+            hostFixture.nativeElement.querySelectorAll('.actions');
+          expect(actions.length).toEqual(0);
+        });
+      });
+
+      describe('has', () => {
+        const action: IntroductionCallToAction = {
+          label: 'Go to blog',
+          onClick: jest.fn(),
+        };
+
+        describe('single action', () => {
+          let actionAnchor: HTMLAnchorElement;
+
+          beforeEach(() => {
+            hostComponent.actions = action;
+            hostFixture.detectChanges();
+            const actions =
+              hostFixture.nativeElement.querySelectorAll('.actions');
+            const [actionElement] = actions;
+            actionAnchor = actionElement?.querySelector('a');
+          });
+
+          it('should render action as anchor', () => {
+            expect(actionAnchor).toBeTruthy();
+          });
+
+          it('should use label in content', () => {
+            expect(actionAnchor.textContent).toContain(action.label);
+          });
+
+          it('should use action onClick when clicked', () => {
+            actionAnchor.click();
+            expect(action.onClick).toHaveBeenCalledWith(expect.any(MouseEvent));
+          });
+
+          it('should default type to "primary"', () => {
+            expect(actionAnchor.classList).toContain('pj-button');
+            expect(actionAnchor.classList).toContain('primary');
+          });
+        });
+
+        describe('multiple actions', () => {
+          let actionAnchors: HTMLAnchorElement[];
+
+          beforeEach(() => {
+            hostComponent.actions = [
+              action,
+              { label: 'Second action', type: 'main', onClick: action.onClick },
+            ];
+            hostFixture.detectChanges();
+            const actions =
+              hostFixture.nativeElement.querySelectorAll('.actions');
+            expect(actions.length).toEqual(1);
+            const [actionElement] = actions;
+            actionAnchors = actionElement.querySelectorAll('a');
+          });
+
+          it('should render actions', () => {
+            expect(actionAnchors.length).toEqual(2);
+            expect(actionAnchors[0].textContent).toContain('Go to blog');
+            expect(actionAnchors[1].textContent).toContain('Second action');
+          });
+
+          it('should use action type for styling', () => {
+            expect(actionAnchors[1].classList).toContain('main');
+          });
+        });
+      });
+    });
   });
 });
--- ng-libs/ui-elements/src/lib/page-introduction/page-introduction.component.ts
+++ ng-libs/ui-elements/src/lib/page-introduction/page-introduction.component.ts
@@ -1,13 +1,69 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from '@angular/core';
 
 import { CommonModule } from '@angular/common';
+import { IntroductionCallToAction } from './models';
 
 @Component({
   selector: 'pj-ui-page-introduction',
   standalone: true,
   imports: [CommonModule],
-  template: `<p>page-introduction works!</p>`,
-  styles: ``,
+  template: `
+    <div
+      class="background flex min-h-screen flex-col items-start"
+      [attr.style]="backgroundStyle()"
+    >
+      <div
+        class="main-colors m-5 items-center rounded border-2 bg-opacity-50 p-12"
+      >
+        <h1 class="mb-8 justify-start text-3xl font-bold">{{ title() }}</h1>
+        @for (paragraph of paragraphs(); track paragraph) {
+          <p class="mb-6 text-xl">{{ paragraph }}</p>
+        }
+        @if (callToActions().length > 0) {
+          <div class="actions mt-8 flex flex-col gap-4">
+            @for (action of callToActions(); track action.label) {
+              <a
+                href="#"
+                class="pj-button {{ action.type || 'primary' }} text-center"
+                (click)="action.onClick($event)"
+              >
+                {{ action.label }}
+              </a>
+            }
+          </div>
+        }
+      </div>
+    </div>
+  `,
+  styles: `
+    .background {
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: top;
+    }
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PageIntroductionComponent {}
+export class PageIntroductionComponent {
+  title = input('👋 Hi there!');
+  backgroundUrl = input('/assets/pug_1280.webp');
+  paragraphs = input<string[]>([]);
+  actions = input<IntroductionCallToAction | IntroductionCallToAction[]>();
+
+  backgroundStyle = computed(
+    () => `background-image: url('${this.backgroundUrl()}')`,
+  );
+  callToActions = computed(() => {
+    const actions = this.actions();
+    if (!actions) {
+      return [];
+    }
+
+    return Array.isArray(actions) ? actions : [actions];
+  });
+}
