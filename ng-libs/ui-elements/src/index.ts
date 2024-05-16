cb55570ca2ad387814619e35b274b1d431ebf29f
Peter Jokumsen <pjokumsen@gmail.com>
2024-05-16 21:38:20 +0200

    fix(ui-elements): move `pj-ui-theme-toggle` into `theme-toggle` folder
    Forgot to update the folder name when decided to alter name of component.

--- ng-libs/ui-elements/src/index.ts
+++ ng-libs/ui-elements/src/index.ts
@@ -2,6 +2,6 @@ export * from './lib/router-nav';
 
 export * from './lib/article';
 
-export * from './lib/theme-selector';
+export * from './lib/theme-toggle';
 
 export * from './lib/page-introduction/page-introduction.component';
