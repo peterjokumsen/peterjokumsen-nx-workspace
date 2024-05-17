da958ef10302ffc9aa5eec6fb8bb9d163b4d7795
Peter Jokumsen <pjokumsen@gmail.com>
2024-05-17 20:31:52 +0200

    feat(blog): update not-found.component to display details

--- apps/blog/src/app/pages/not-found/not-found.component.spec.ts
+++ apps/blog/src/app/pages/not-found/not-found.component.spec.ts
@@ -1,15 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MockComponent } from 'ng-mocks';
 import { NotFoundComponent } from './not-found.component';
+import { PageIntroductionComponent } from '@peterjokumsen/ui-elements';
+import { PjLogger } from '@peterjokumsen/ng-services';
+import { Router } from '@angular/router';
 
 describe('NotFoundComponent', () => {
   let component: NotFoundComponent;
   let fixture: ComponentFixture<NotFoundComponent>;
+  let routerSpy: jest.Mocked<Router>;
 
   beforeEach(async () => {
+    routerSpy = {
+      navigate: jest.fn(() => Promise.resolve(true)),
+    } as unknown as jest.Mocked<Router>;
+
     await TestBed.configureTestingModule({
       imports: [NotFoundComponent],
-    }).compileComponents();
+      providers: [
+        { provide: PjLogger, useValue: { to: { warn: jest.fn() } } },
+        { provide: Router, useValue: routerSpy },
+      ],
+    })
+      .overrideComponent(NotFoundComponent, {
+        set: {
+          imports: [MockComponent(PageIntroductionComponent)],
+        },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(NotFoundComponent);
     component = fixture.componentInstance;
