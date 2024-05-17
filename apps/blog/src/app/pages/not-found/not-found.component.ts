fde7659685807b390268e2ad798e5a5a26be5f96
Peter Jokumsen <pjokumsen@gmail.com>
2024-05-17 20:31:52 +0200

    feat(blog): update not-found.component to display details

--- apps/blog/src/app/pages/not-found/not-found.component.ts
+++ apps/blog/src/app/pages/not-found/not-found.component.ts
@@ -1,12 +1,47 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
+import {
+  IntroductionBackgroundStyle,
+  PageIntroductionComponent,
+} from '@peterjokumsen/ui-elements';
 
 import { CommonModule } from '@angular/common';
+import { PjLogger } from '@peterjokumsen/ng-services';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>not-found works!</p>`,
+  imports: [CommonModule, PageIntroductionComponent],
+  template: `
+    <pj-ui-page-introduction
+      title="🫣 Oops!"
+      [paragraphs]="paragraphs"
+      [style]="backgroundStyle"
+      [actions]="[{ label: 'Go home' }]"
+      (callToAction)="router.navigate(['/'])"
+    ></pj-ui-page-introduction>
+  `,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NotFoundComponent {}
+export class NotFoundComponent implements OnInit {
+  private _logger = inject(PjLogger);
+
+  router = inject(Router);
+
+  backgroundStyle: IntroductionBackgroundStyle = {
+    url: '/assets/not_found_background.webp',
+  };
+  paragraphs = ['The page you are looking for seems to be missing.'];
+
+  ngOnInit() {
+    this._logger.to.warn(
+      'User tried to access a non-existing page: "%s"',
+      this.router.url,
+    );
+  }
+}
