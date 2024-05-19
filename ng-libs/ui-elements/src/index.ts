0bec08736cac8d14b96b6d07309239374d599683
Peter Jokumsen <pjokumsen@gmail.com>
2024-05-19 15:16:59 +0200

    feat(ui-elements): create full-page-loader component

--- ng-libs/ui-elements/src/index.ts
+++ ng-libs/ui-elements/src/index.ts
@@ -1,7 +1,5 @@
-export * from './lib/router-nav';
-
 export * from './lib/article';
-
-export * from './lib/theme-toggle';
-
 export * from './lib/page-introduction';
+export * from './lib/full-page-loader';
+export * from './lib/router-nav';
+export * from './lib/theme-toggle';
