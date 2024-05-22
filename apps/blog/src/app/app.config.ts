56e12e752d26894f5f1eea0eb64b5c1294ba0bd5
Peter Jokumsen <pjokumsen@gmail.com>
2024-05-22 22:34:35 +0200

    feat(blog): set up project as PWA
    Using `nx g @angular/pwa:ng-add --project=blog`

--- apps/blog/src/app/app.config.ts
+++ apps/blog/src/app/app.config.ts
@@ -9,6 +9,7 @@ import { appRoutes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,5 +19,9 @@ export const appConfig: ApplicationConfig = {
     providePjBrowserProviders(),
     providePjLogger({ production: !isDevMode() }),
     providePjTheme(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
