4337297fe0f4f78d9b893031d43aad6acee0130e
Peter Jokumsen <pjokumsen@gmail.com>
2024-05-24 00:24:04 +0200

    feat(blog): update change-history to use markdown

--- apps/blog/src/app/components/change-history/change-history.component.spec.ts
+++ apps/blog/src/app/components/change-history/change-history.component.spec.ts
@@ -1,16 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  PjArticle,
+  PjArticleParser,
+  PjLogger,
+} from '@peterjokumsen/ng-services';
 
 import { ArticleComponent } from '@peterjokumsen/ui-elements';
 import { ChangeHistoryComponent } from './change-history.component';
 import { MockComponent } from 'ng-mocks';
+import { of } from 'rxjs';
 
 describe('ChangeHistoryComponent', () => {
   let component: ChangeHistoryComponent;
   let fixture: ComponentFixture<ChangeHistoryComponent>;
+  let parserSpy: jest.Mocked<PjArticleParser>;
 
   beforeEach(async () => {
+    parserSpy = jest.mocked<Partial<PjArticleParser>>({
+      fromSource: jest.fn(),
+    }) as unknown as jest.Mocked<PjArticleParser>;
+    parserSpy.fromSource.mockReturnValue(
+      of<PjArticle>({ sections: [{ title: 'test', content: 'testing' }] }),
+    );
+
     await TestBed.configureTestingModule({
       imports: [ChangeHistoryComponent],
+      providers: [
+        // keep split
+        { provide: PjArticleParser, useValue: parserSpy },
+      ],
     })
       .overrideComponent(ChangeHistoryComponent, {
         set: {
@@ -26,5 +44,14 @@ describe('ChangeHistoryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should use the parser to parse the article', () => {
+    expect(parserSpy.fromSource).toHaveBeenCalledWith(
+      expect.stringContaining('something'),
+    );
+    expect(component.article()).toEqual({
+      sections: [{ title: 'test', content: 'testing' }],
+    });
   });
 });
