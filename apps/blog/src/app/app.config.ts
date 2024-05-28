8a88c5c60152c13387b66a012e8eb38336ef7513
Peter Jokumsen <pjokumsen@gmail.com>
2024-05-29 00:35:41 +0200

    feat(blog): update to use @angular/material

--- apps/blog/src/app/app.config.ts
+++ apps/blog/src/app/app.config.ts
@@ -7,14 +7,14 @@ import {
 } from '@peterjokumsen/ng-services';
 
 import { appRoutes } from './app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideAnimations(),
+    provideAnimationsAsync(),
     provideClientHydration(),
     provideRouter(appRoutes),
     providePjBrowserProviders(),
