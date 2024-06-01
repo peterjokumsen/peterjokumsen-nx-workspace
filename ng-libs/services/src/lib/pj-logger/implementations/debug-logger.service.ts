cfda1339b0f26c4497506858902ecc6f98bbb284
Peter Jokumsen <pjokumsen@gmail.com>
2024-06-01 03:12:02 +0200

    feat(ng-services): update debug implementation of `PjLogger`
    Updated the logger implementation to include timing for calls to group.

--- ng-libs/services/src/lib/pj-logger/implementations/debug-logger.service.ts
+++ ng-libs/services/src/lib/pj-logger/implementations/debug-logger.service.ts
@@ -4,7 +4,23 @@ import { PjLogger } from '../pj-logger.service';
 
 @Injectable()
 export class DebugLoggerService extends PjLogger {
-  private readonly _console: LogFns = console;
+  private _titles: string[] = [];
+  private readonly _console: LogFns = {
+    ...console,
+    group: (title) => {
+      console.group(title);
+      console.time(title);
+      this._titles.push(title);
+    },
+    groupEnd: () => {
+      if (this._titles.length > 0) {
+        const title = this._titles.pop();
+        console.timeEnd(title);
+      }
+
+      console.groupEnd();
+    },
+  };
 
   get to() {
     return this._console;
