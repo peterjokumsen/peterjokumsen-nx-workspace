753877cb515ef083aa58bd2a55c654d298cb0e4c
Peter Jokumsen <pjokumsen@gmail.com>
2024-06-10 00:20:38 +0200

    feat(blog): update header component to show secondary header

--- apps/blog/src/app/components/header/header.component.spec.ts
+++ apps/blog/src/app/components/header/header.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockComponent, MockDirective } from 'ng-mocks';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { HeaderComponent } from './header.component';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterNavComponent } from '@peterjokumsen/ui-elements';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -9,7 +14,18 @@ describe('HeaderComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HeaderComponent],
-    }).compileComponents();
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+      .overrideComponent(HeaderComponent, {
+        set: {
+          imports: [
+            MockComponent(RouterNavComponent),
+            MockComponent(FaIconComponent),
+            MockDirective(NgOptimizedImage),
+          ],
+        },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
--- apps/blog/src/app/components/header/header.component.ts
+++ apps/blog/src/app/components/header/header.component.ts
@@ -1,12 +1,30 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild,
+  inject,
+  input,
+  signal,
+} from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { PjBrowserTools, PjLogger } from '@peterjokumsen/ng-services';
 import {
   PjUiRouterNavigationElement,
   RouterNavComponent,
 } from '@peterjokumsen/ui-elements';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { faChevronRight, faCode } from '@fortawesome/free-solid-svg-icons';
 
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { MatFabButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
@@ -16,52 +34,76 @@ import { faCode } from '@fortawesome/free-solid-svg-icons';
     FaIconComponent,
     NgOptimizedImage,
     RouterNavComponent,
+    MatFabButton,
   ],
-  template: `
-    <header class="primary-block pj-header content">
-      <div class="pj-header-title content">
-        <div class="pj-header-title-logo">
-          <img
-            ngSrc="assets/logo-150.webp"
-            alt="logo"
-            height="150"
-            width="150"
-            [priority]="true"
-          />
-        </div>
-        <div class="pj-header-title-heading">
-          <h1>Peter Jokumsen</h1>
-          <h2 class="alt-text">An intentional software developer</h2>
-          <p>
-            <fa-icon class="mr-2" [icon]="codeIcon" aria-label="Code"></fa-icon>
-            using
-            <img
-              class="mx-2"
-              ngSrc="assets/icons/c-sharp.svg"
-              width="32"
-              height="32"
-              alt="C#"
-              aria-label="C Sharp"
-            />
-            &amp;
-            <img
-              class="ml-2"
-              ngSrc="assets/icons/typescript.svg"
-              width="32"
-              height="32"
-              alt="Typescript"
-              aria-label="Typescript"
-            />
-          </p>
-        </div>
-      </div>
-      <pj-ui-router-nav [routes]="navElements()"></pj-ui-router-nav>
-    </header>
+  templateUrl: './header.component.html',
+  styles: `
+    .static-header {
+      position: fixed;
+      top: 0;
+    }
+
+    .secondary-header {
+      height: 70px;
+    }
   `,
-  styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('showHide', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('0.4s', style({ transform: 'translateX(0)' })),
+      ]),
+      transition(':leave', [
+        animate('300ms', style({ transform: 'translateX(-100%)' })),
+      ]),
+    ]),
+    trigger('expandOrCollapse', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('0.4s', style({ transform: 'translateX(0)' })),
+      ]),
+      transition(':leave', [
+        animate('300ms', style({ transform: 'translateX(-100%)' })),
+      ]),
+    ]),
+    trigger('rotateExpanded', [
+      state('true', style({ transform: 'rotate(180deg)' })),
+      state('false', style({ transform: 'rotate(0deg)' })),
+      transition('true <=> false', animate('0.4s')),
+    ]),
+  ],
 })
 export class HeaderComponent {
+  private _logger = inject(PjLogger, { optional: true });
+  private _browserTools = inject(PjBrowserTools, { optional: true });
+
+  imageSrc = 'assets/logo-150.webp';
+  windowPastHeader = signal<boolean>(false);
+
   codeIcon = faCode;
   navElements = input<PjUiRouterNavigationElement[]>([]);
+
+  expanded = false;
+  chevronIcon = faChevronRight;
+
+  @ViewChild('primaryHeader', { static: true }) primaryHeader?: ElementRef;
+
+  @HostListener('window:scroll')
+  onScroll() {
+    const windowScrollY = this._browserTools?.window?.scrollY ?? 0;
+    const documentHeight =
+      this._browserTools?.window?.document?.documentElement.clientHeight ?? 0;
+    const headerHeight = this.primaryHeader?.nativeElement.clientHeight ?? 0;
+    const pastHeader = windowScrollY > documentHeight + headerHeight;
+    if (this.windowPastHeader() !== pastHeader) {
+      this._logger?.to.log(
+        'Window: %s, Document: %s, Header: %s',
+        windowScrollY,
+        documentHeight,
+        headerHeight,
+      );
+      this.windowPastHeader.update(() => pastHeader);
+    }
+  }
 }
