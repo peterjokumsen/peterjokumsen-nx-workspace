91a53859387c197ca24abc11b60165c0b5aee3c3
Peter Jokumsen <pjokumsen@gmail.com>
2024-06-10 02:40:06 +0200

    chore(ng-services): remove logging in pj-article-parser

--- ng-libs/services/src/lib/pj-article-parser/pj-article-parser.service.ts
+++ ng-libs/services/src/lib/pj-article-parser/pj-article-parser.service.ts
@@ -9,20 +9,16 @@ export class PjArticleParser {
   private _logger = inject(PjLogger, { optional: true });
 
   fromSource(source: string): Observable<PjArticle> {
-    this._logger?.to.group('PJ Article Parser');
     const parsedResult: PjArticle = {
       sections: [],
     };
     let content: PjArticleContent = [];
     for (const rawLine of source.split('\n').reverse()) {
       const line = rawLine?.trimStart();
-      this._logger?.to.log('rawLine: "%s"', rawLine);
-      this._logger?.to.log('line: "%s"', line);
       if (line.startsWith('# ')) {
         parsedResult.title = line.replace('# ', '');
         parsedResult.introduction = [...content.reverse()];
         parsedResult.sections.reverse();
-        this._logger?.to.log('found title: %s', parsedResult.title);
         break;
       }
 
@@ -44,8 +40,6 @@ export class PjArticleParser {
       }
     }
 
-    this._logger?.to.log('parseResult: %o', parsedResult);
-    this._logger?.to.groupEnd();
     return of(parsedResult);
   }
 }
