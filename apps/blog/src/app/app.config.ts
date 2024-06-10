abad609a9a65feba7b42c2bfec361a48f94aef61
Peter Jokumsen <pjokumsen@gmail.com>
2024-06-10 23:28:35 +0200

    feat(blog): create and use splash component

--- apps/blog/src/app/app.config.ts
+++ apps/blog/src/app/app.config.ts
@@ -4,35 +4,20 @@ import {
   provideRouter,
   withPreloading,
 } from '@angular/router';
-import {
-  providePjArticleParser,
-  providePjBrowserTools,
-  providePjHttpTools,
-  providePjLogger,
-  providePjTheme,
-} from '@peterjokumsen/ng-services';
 
-import { appRoutes } from './app.routes';
+import { appInitialRoutes } from './app-initial.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
-import { withFetch } from '@angular/common/http';
-
-const config = { production: !isDevMode() };
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
     provideClientHydration(),
-    provideRouter(appRoutes, withPreloading(PreloadAllModules)),
-    providePjHttpTools(config, withFetch()),
-    providePjLogger(config),
-    providePjTheme(),
+    provideRouter(appInitialRoutes, withPreloading(PreloadAllModules)),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
     }),
-    providePjArticleParser(),
-    providePjBrowserTools(),
   ],
 };
