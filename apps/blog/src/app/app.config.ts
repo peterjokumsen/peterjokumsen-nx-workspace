9be3570662401b4d37cd6d9e156208d8f6dc3f13
Peter Jokumsen <pjokumsen@gmail.com>
2024-06-11 23:24:54 +0200

    fix: get specs passing

--- apps/blog/src/app/app.config.ts
+++ apps/blog/src/app/app.config.ts
@@ -3,8 +3,10 @@ import { ApplicationConfig, isDevMode } from '@angular/core';
 import { appInitialRoutes } from './app-initial.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration } from '@angular/platform-browser';
+import { providePjHttpTools } from '@peterjokumsen/ng-services';
 import { provideRouter } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
+import { withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +17,6 @@ export const appConfig: ApplicationConfig = {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    providePjHttpTools({ production: !isDevMode() }, withFetch()),
   ],
 };
