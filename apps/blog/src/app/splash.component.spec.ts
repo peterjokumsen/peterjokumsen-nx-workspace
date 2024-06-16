416bbff46a8fec35317351f5cf31173e5e9a2ebf
Peter Jokumsen <pjokumsen@gmail.com>
2024-06-16 21:10:55 +0200

    feat(blog): set first theme in `SplashComponent`
    Initial load seems to have been reduced from these changes, to 346kb

--- apps/blog/src/app/splash.component.spec.ts
+++ apps/blog/src/app/splash.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { PjTheme } from '@peterjokumsen/ng-services';
 import { SplashComponent } from './splash.component';
+import { of } from 'rxjs';
 
 describe('SplashComponent', () => {
   let component: SplashComponent;
   let fixture: ComponentFixture<SplashComponent>;
+  let themeSpy: Partial<jest.Mocked<PjTheme>>;
 
   beforeEach(async () => {
+    themeSpy = {
+      theme$: of('dark'),
+      setTheme: jest.fn(),
+    };
     await TestBed.configureTestingModule({
       imports: [SplashComponent],
+      providers: [{ provide: PjTheme, useValue: themeSpy }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SplashComponent);
@@ -18,5 +26,9 @@ describe('SplashComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set theme', () => {
+    expect(themeSpy.setTheme).toHaveBeenCalledWith('dark');
   });
 });
--- apps/blog/src/app/splash.component.ts
+++ apps/blog/src/app/splash.component.ts
@@ -1,20 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule, NgOptimizedImage } from '@angular/common';
 import {
-  animate,
-  keyframes,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { animate, style, transition, trigger } from '@angular/animations';
 
-import { PrimaryComponent } from './primary.component';
+import { PjTheme } from '@peterjokumsen/ng-services';
 import { RouterOutlet } from '@angular/router';
+import { first } from 'rxjs';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, NgOptimizedImage, PrimaryComponent],
+  imports: [CommonModule, RouterOutlet, NgOptimizedImage],
   animations: [
     trigger('fadeOut', [
       transition(':leave', [
@@ -23,17 +23,18 @@ import { RouterOutlet } from '@angular/router';
       ]),
     ]),
   ],
+  providers: [],
   template: `
     @defer {
       <router-outlet></router-outlet>
     } @loading (minimum 500ms) {
       <div @fadeOut class="splash">
         <img
-          @coinFlip
           ngSrc="/assets/logo-150.webp"
           width="150"
           height="150"
           alt="Splash logo"
+          [priority]="true"
         />
       </div>
     }
@@ -69,4 +70,12 @@ import { RouterOutlet } from '@angular/router';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SplashComponent {}
+export class SplashComponent implements OnInit {
+  private _themeSvc = inject(PjTheme);
+
+  ngOnInit() {
+    this._themeSvc.theme$.pipe(first()).subscribe((theme) => {
+      this._themeSvc.setTheme(theme);
+    });
+  }
+}
