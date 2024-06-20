f4acc0fc75c013267f8c26be986c0e54a5a1f7b4
Peter Jokumsen <pjokumsen@gmail.com>
2024-06-20 22:32:06 +0200

    feat(md-renderer): update md-content service id generation
    Use `title` for generating id, otherwise include `type` from passed in object or just use `id` as default.

--- ng-libs/md-renderer/src/lib/services/md-content.service.ts
+++ ng-libs/md-renderer/src/lib/services/md-content.service.ts
@@ -4,11 +4,36 @@ import { WithId } from '../models';
 
 @Injectable()
 export class MdContentService {
-  private _count = 0;
+  private _countById: Record<string, number> = {};
+
+  private hasType(obj: object): obj is { type: string } {
+    return 'type' in obj;
+  }
+
+  private hasTitle(obj: object): obj is { title: string } {
+    return 'title' in obj;
+  }
 
   addId<T extends object>(obj: T): WithId<T> {
     if ('id' in obj) return obj as T & { id: string };
-    return Object.assign(obj, { id: `id_${this._count++}` });
+    let id = 'id';
+    let showZero = true;
+    if (this.hasTitle(obj)) {
+      id = obj.title?.toLowerCase().replace(/\W/g, '-');
+      showZero = false;
+    }
+
+    if (this.hasType(obj)) {
+      id = obj.type;
+    }
+
+    const count = this._countById[id] ?? 0;
+    this._countById[id] = count + 1;
+    if (count > 0 || showZero) {
+      id += `_${count}`;
+    }
+
+    return Object.assign(obj, { id });
   }
 
   mapContent(value: string | MarkdownContent): WithId<MarkdownContent> {
