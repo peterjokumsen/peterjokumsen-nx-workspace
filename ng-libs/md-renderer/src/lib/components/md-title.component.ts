c7d364527f1f69d5e26af212d7970d2542249cc2
Peter Jokumsen <pjokumsen@gmail.com>
2024-07-03 21:16:00 +0200

    feat(md-renderer): add style for title component
    Set all header elements to have `bold` font weight, and apply font-size for each header level:
    - 1 - `2em`
    - 2 - `1.8em`
    - 3 - `1.6em`
    - 4 - `1.4em`
    - 5 - `1.2em`
    - 6 - `1.1em`
    
    Randomly selected sizing... may need adjustment in the future.

--- ng-libs/md-renderer/src/lib/components/md-title.component.ts
+++ ng-libs/md-renderer/src/lib/components/md-title.component.ts
@@ -48,7 +48,40 @@ import { WithId } from '../models';
       }
     }
   `,
-  styles: ``,
+  styles: `
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      font-weight: bold;
+    }
+
+    h1 {
+      font-size: 2em;
+    }
+
+    h2 {
+      font-size: 1.8em;
+    }
+
+    h3 {
+      font-size: 1.6em;
+    }
+
+    h4 {
+      font-size: 1.4em;
+    }
+
+    h5 {
+      font-size: 1.2em;
+    }
+
+    h6 {
+      font-size: 1.1em;
+    }
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MdTitleComponent {
