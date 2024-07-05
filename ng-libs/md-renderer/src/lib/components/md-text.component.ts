40751ad0309b83f9b57c1836fbbc42d85178ff86
Peter Jokumsen <pjokumsen@gmail.com>
2024-07-06 00:47:08 +0200

    feat(md-renderer): update md-text to include formatting

--- ng-libs/md-renderer/src/lib/components/md-text.component.ts
+++ ng-libs/md-renderer/src/lib/components/md-text.component.ts
@@ -14,12 +14,33 @@ import { mdModelCheck } from '@peterjokumsen/ts-md-models';
   selector: 'pj-mdr-md-text',
   template: `
     @if (textValue()) {
-      <ng-container>{{ textValue() }}</ng-container>
+      <span [class]="class()">{{ textValue() }}</span>
     }
   `,
   styles: `
     :host {
       display: inline;
+    }
+
+    .bold {
+      font-weight: bold;
+    }
+
+    .italic {
+      font-style: italic;
+    }
+
+    .strike-through {
+      text-decoration: line-through;
+    }
+
+    .bold-italic {
+      font-weight: bold;
+      font-style: italic;
+    }
+
+    .line-break {
+      white-space: pre-line;
     }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -27,17 +48,21 @@ import { mdModelCheck } from '@peterjokumsen/ts-md-models';
 export class MdTextComponent implements HasContent<'text'> {
   private _logger = inject(PjLogger, { optional: true });
   textValue = signal<string | null>(null);
+  class = signal<string>('');
 
   set content(value: HasContent<'text'>['content']) {
     let newContent: string | null = null;
+    let classValue: string | null = null;
     if (typeof value == 'string') {
       newContent = value;
     } else if (mdModelCheck('text', value)) {
       newContent = value.content;
+      classValue = value.format ?? '';
     } else {
       logUnexpectedContent('MdTextComponent', value, this._logger?.to);
     }
 
     this.textValue.update(() => newContent);
+    this.class.update(() => classValue ?? '');
   }
 }
