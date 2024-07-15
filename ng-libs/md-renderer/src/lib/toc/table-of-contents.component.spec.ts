26ec2d79733bf8cc956d1cb93b1edc8f97f14037
Peter Jokumsen <pjokumsen@gmail.com>
2024-07-15 23:04:28 +0200

    chore(md-renderer): add test for `onScroll` in table-of-contents

--- ng-libs/md-renderer/src/lib/toc/table-of-contents.component.spec.ts
+++ ng-libs/md-renderer/src/lib/toc/table-of-contents.component.spec.ts
@@ -1,15 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { PjBrowserTools } from '@peterjokumsen/ng-services';
 import { TableOfContentsComponent } from './table-of-contents.component';
 
 describe('TableOfContentsComponent', () => {
   let component: TableOfContentsComponent;
   let fixture: ComponentFixture<TableOfContentsComponent>;
+  let browserToolsSpy: Partial<jest.Mocked<PjBrowserTools>>;
+  let windowSpy: Partial<jest.Mocked<Window>>;
 
   beforeEach(async () => {
+    windowSpy = {
+      scrollY: 250,
+    };
+    browserToolsSpy = {
+      window: windowSpy as unknown as Window,
+    };
     await TestBed.configureTestingModule({
       imports: [TableOfContentsComponent, NoopAnimationsModule],
+      providers: [
+        // providers
+        { provide: PjBrowserTools, useValue: browserToolsSpy },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TableOfContentsComponent);
@@ -19,5 +32,13 @@ describe('TableOfContentsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('onScroll', () => {
+    it('should update scroll height', () => {
+      component.inViewSectionId.apply(() => 'section');
+      component.onScroll();
+      expect(component.showBackToTop()).toBe(true);
+    });
   });
 });
