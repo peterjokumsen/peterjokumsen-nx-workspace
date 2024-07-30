2834bb050ca43c156e1b03d5d2854fb6ff042078
Peter Jokumsen <pjokumsen@gmail.com>
2024-07-30 22:00:57 +0200

    feat!(blog): use md-renderer in `change-history`

--- apps/blog/src/app/components/change-history/change-history.component.ts
+++ apps/blog/src/app/components/change-history/change-history.component.ts
@@ -1,19 +1,14 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  computed,
-  inject,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
-import { ArticleComponent } from '@peterjokumsen/ui-elements';
 import { CommonModule } from '@angular/common';
+import { MdRendererComponent } from '@peterjokumsen/md-renderer';
 import { PjArticleParser } from '@peterjokumsen/ng-services';
 import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-change-history',
   standalone: true,
-  imports: [CommonModule, ArticleComponent],
+  imports: [CommonModule, MdRendererComponent],
   templateUrl: './change-history.component.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,9 +17,10 @@ export class ChangeHistoryComponent {
   private _parser = inject(PjArticleParser);
 
   private readonly _markdown = `
-## Work in progress
+# Work in progress
 
 There are quite a number of pieces I would like to get in place to complete this page. Some of the things I am considering are:
+
 - A process to publish documentation of projects in this workspace
 - Notes on process applied to get this blog up and running
 - Notes on the development and projects done for this blog
@@ -32,6 +28,5 @@ There are quite a number of pieces I would like to get in place to complete this
 - Records of changes made to the blog
 `;
 
-  article = toSignal(this._parser.fromSource(this._markdown));
-  sections = computed(() => this.article()?.sections ?? []);
+  parsedMarkdown = toSignal(this._parser.fromSource(this._markdown));
 }
