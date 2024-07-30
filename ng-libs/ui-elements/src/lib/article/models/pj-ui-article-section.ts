5da5ee398faa862aa89a3f9f28968e764d0b13ef
Peter Jokumsen <pjokumsen@gmail.com>
2024-07-30 22:07:41 +0200

    chore(ui-elements): remove article components

--- ng-libs/ui-elements/src/lib/article/models/pj-ui-article-section.ts
+++
@@ -1,7 +0,0 @@
-import { PjArticleSection } from '@peterjokumsen/ng-services';
-import { PjUiContent } from './pj-ui-content';
-
-export interface PjUiArticleSection extends PjArticleSection {
-  title: string;
-  content: PjUiContent;
-}
