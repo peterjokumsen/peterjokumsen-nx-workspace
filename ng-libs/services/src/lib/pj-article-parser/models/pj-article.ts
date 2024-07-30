7bae4e1e0a6234f6d4bc2672024f404a03ef14cb
Peter Jokumsen <pjokumsen@gmail.com>
2024-07-30 21:48:57 +0200

    feat!(ng-services): use `parseMarkdown` in `pj-article-parser`

--- ng-libs/services/src/lib/pj-article-parser/models/pj-article.ts
+++
@@ -1,8 +0,0 @@
-import { PjArticleContent } from './pj-article-content';
-import { PjArticleSection } from './pj-article-section';
-
-export interface PjArticle {
-  title?: string;
-  introduction?: PjArticleContent;
-  sections: PjArticleSection[];
-}
--- ng-libs/services/src/lib/pj-article-parser/pj-article-parser.service.ts
+++ ng-libs/services/src/lib/pj-article-parser/pj-article-parser.service.ts
@@ -1,45 +1,15 @@
 import { Injectable, inject } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { PjArticle, PjArticleContent } from './models';
 
+import { MarkdownAst } from '@peterjokumsen/ts-md-models';
 import { PjLogger } from '../pj-logger';
+import { parseMarkdown } from '@peterjokumsen/md-parser';
 
 @Injectable()
 export class PjArticleParser {
   private _logger = inject(PjLogger, { optional: true });
 
-  fromSource(source: string): Observable<PjArticle> {
-    const parsedResult: PjArticle = {
-      sections: [],
-    };
-    let content: PjArticleContent = [];
-    for (const rawLine of source.split('\n').reverse()) {
-      const line = rawLine?.trimStart();
-      if (line.startsWith('# ')) {
-        parsedResult.title = line.replace('# ', '');
-        parsedResult.introduction = [...content.reverse()];
-        parsedResult.sections.reverse();
-        break;
-      }
-
-      if (line.startsWith('## ')) {
-        parsedResult.sections.push({
-          title: line.replace('## ', ''),
-          content: [...content.reverse()],
-        });
-        this._logger?.to.log(
-          'found section: %o',
-          parsedResult.sections[parsedResult.sections.length - 1],
-        );
-        content = [];
-        continue;
-      }
-
-      if (line.trim().length > 0) {
-        content.push(line);
-      }
-    }
-
-    return of(parsedResult);
+  fromSource(markdown: string): Observable<MarkdownAst> {
+    return of(parseMarkdown(markdown));
   }
 }
