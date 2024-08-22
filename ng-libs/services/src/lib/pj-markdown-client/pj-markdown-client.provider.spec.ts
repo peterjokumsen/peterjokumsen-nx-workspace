08533ac89c420a466d668d938829969993f9317d
Peter Jokumsen <pjokumsen@gmail.com>
2024-08-23 00:06:55 +0200

    feat(ng-services): provide MarkdownParserService

--- ng-libs/services/src/lib/pj-markdown-client/pj-markdown-client.provider.spec.ts
+++ ng-libs/services/src/lib/pj-markdown-client/pj-markdown-client.provider.spec.ts
@@ -1,6 +1,7 @@
 import { PjMarkdownClient, providePjMarkdownClient } from './';
 
 import { TestBed } from '@angular/core/testing';
+import { MarkdownParserService } from './services';
 
 describe('providePjTheme', () => {
   beforeEach(() => {
@@ -11,5 +12,9 @@ describe('providePjTheme', () => {
 
   it('should provide PjMarkdownClient service', () => {
     expect(TestBed.inject(PjMarkdownClient)).toBeTruthy();
+  });
+
+  it('should provide MarkdownParserService service', () => {
+    expect(TestBed.inject(MarkdownParserService)).toBeTruthy();
   });
 });
--- ng-libs/services/src/lib/pj-markdown-client/pj-markdown-client.provider.ts
+++ ng-libs/services/src/lib/pj-markdown-client/pj-markdown-client.provider.ts
@@ -1,6 +1,11 @@
 import { PjMarkdownClient } from './pj-markdown-client.service';
 import { Provider } from '@angular/core';
+import { MarkdownParserService } from './services';
 
 export function providePjMarkdownClient(): Provider[] {
-  return [PjMarkdownClient];
+  return [
+    // keep split
+    MarkdownParserService,
+    PjMarkdownClient,
+  ];
 }
