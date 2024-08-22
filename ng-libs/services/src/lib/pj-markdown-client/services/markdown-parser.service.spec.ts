58ef8d1bb41a5216b7426ec47a742e9bffbf598e
Peter Jokumsen <pjokumsen@gmail.com>
2024-08-23 00:04:08 +0200

    feat(md-renderer): create parser to handle image paths

---
+++ ng-libs/services/src/lib/pj-markdown-client/services/markdown-parser.service.spec.ts
@@ -0,0 +1,71 @@
+import { TestBed } from '@angular/core/testing';
+
+import { MarkdownParserService } from './markdown-parser.service';
+import { parseMarkdown } from '@peterjokumsen/md-parser';
+
+jest.mock('@peterjokumsen/md-parser');
+
+describe('MarkdownParserService', () => {
+  let service: MarkdownParserService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        // keep split
+        MarkdownParserService,
+      ],
+    });
+    service = TestBed.inject(MarkdownParserService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('parse', () => {
+    let parseMock: jest.MockedFn<typeof parseMarkdown>;
+
+    beforeEach(() => {
+      parseMock = jest.mocked(parseMarkdown)
+        .mockName('parseMarkdown')
+        .mockReturnValue({ sections: [], tags: [] });
+    });
+
+    afterEach(() => {
+      parseMock.mockReset();
+    });
+
+    it('should call parseMarkdown with the markdown content', () => {
+      const markdownContent = 'markdown content';
+
+      service.parse({ markdownContent, basePath: '/assets' });
+
+      expect(parseMock).toHaveBeenCalledWith(markdownContent);
+    });
+
+    describe('when content has relative image paths', () => {
+      it.each([
+        ['image.png', '/assets/doc/image.png'],
+        ['./image.png', '/assets/doc/image.png'],
+        ['../images/image.png', '/assets/images/image.png'],
+        ['../../images/image.png', '/images/image.png'],
+      ])('should replace "%s" with "%s" when base path is "assets/doc"', (original, expected) => {
+        const markdownContent = `![alt](${original})`;
+
+        service.parse({ markdownContent, basePath: 'assets/doc' });
+
+        expect(parseMock).toHaveBeenCalledWith(`![alt](${expected})`);
+      });
+    });
+
+    describe('when content has absolute image paths', () => {
+      it('should not modify them', () => {
+        const markdownContent = '![image](https://test.com/image.png)';
+
+        service.parse({ markdownContent, basePath: '/assets' });
+
+        expect(parseMock).toHaveBeenCalledWith(markdownContent);
+      });
+    });
+  });
+});
---
+++ ng-libs/services/src/lib/pj-markdown-client/services/markdown-parser.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { parseMarkdown } from '@peterjokumsen/md-parser';
+import { ToBeParsed } from '../models';
+import { MarkdownAst } from '@peterjokumsen/ts-md-models';
+
+/**
+ * Service to parse markdown content, using parseMarkdown after adjusting the content as follows:
+ * - Replace any relative image paths with paths relative to the provided base path.
+ * - TBD: Add more adjustments as needed.
+ *
+ * Not intended to be used directly, but rather through the PjMarkdownClient.
+ */
+@Injectable()
+export class MarkdownParserService {
+  /**
+   * Replace any relative image paths with paths relative to the provided base path.
+   * @param markdownContent
+   * @param basePath
+   * @private
+   */
+  private transformRelativeImagePaths(markdownContent: string, basePath: string): string {
+    const regex = /!\[(.*)]\(((?:\.{1,2}\/)*)?(\w+\/)*(\w+.\w+)\)/g;
+    return markdownContent.replace(regex, (_, altText, relativePath, folderPath, fileName) => {
+      let base = basePath[0] === '/' ? basePath.slice(1) : basePath;
+      if (typeof relativePath === 'string' && relativePath.includes('..')) {
+        const baseParts = basePath.split('/').filter((p) => !!p);
+        const relativeParts = relativePath.split('/').filter((p) => !!p);
+        const parts = baseParts.slice(0, baseParts.length - relativeParts.length);
+        base = `${parts.join('/')}`;
+      }
+
+      const path = `/${base}/${folderPath ?? ''}`.replace(/\/+/g, '/');
+      return `![${altText}](${path}${fileName})`;
+    });
+  }
+
+  /**
+   * Parse the markdown content.
+   * @param markdownContent
+   * @param basePath
+   */
+  parse({ markdownContent, basePath }: ToBeParsed): MarkdownAst {
+    markdownContent = this.transformRelativeImagePaths(markdownContent, basePath);
+    return parseMarkdown(markdownContent);
+  }
+}
