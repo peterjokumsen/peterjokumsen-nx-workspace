690ca430c77dda8946c7362673841e53e37c8c4f
Peter Jokumsen <pjokumsen@gmail.com>
2024-08-24 23:41:18 +0200

    show loader while waiting for markdown to display

--- apps/blog/src/app/pages/about-me/about-me.component.ts
+++ apps/blog/src/app/pages/about-me/about-me.component.ts
@@ -3,15 +3,31 @@ import { PjLogger, PjMarkdownClient } from '@peterjokumsen/ng-services';
 
 import { CommonModule } from '@angular/common';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { MdRendererComponent } from '@peterjokumsen/md-renderer';
 import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [CommonModule, FaIconComponent, MdRendererComponent],
+  imports: [
+    CommonModule,
+    FaIconComponent,
+    MdRendererComponent,
+    MatProgressSpinner,
+  ],
   template: `
-    <pj-mdr-md-renderer [parsedContent]="markdown()"></pj-mdr-md-renderer>
+    @if (markdown()) {
+      <pj-mdr-md-renderer [parsedContent]="markdown()"></pj-mdr-md-renderer>
+    } @else {
+      <div class="loading">
+        @defer {
+          <mat-progress-spinner [mode]="'indeterminate'"></mat-progress-spinner>
+        } @placeholder {
+          <p>Loading...</p>
+        }
+      </div>
+    }
   `,
   styleUrl: 'about-me.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
