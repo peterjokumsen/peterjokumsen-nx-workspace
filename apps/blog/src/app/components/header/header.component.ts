279098d177e1f26e1f5782a1f0da6e863943e733
Peter Jokumsen <pjokumsen@gmail.com>
2024-08-28 23:02:58 +0200

    feat(blog): display loading component in header

--- apps/blog/src/app/components/header/header.component.ts
+++ apps/blog/src/app/components/header/header.component.ts
@@ -1,12 +1,22 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed, inject,
+  input,
+  signal,
+} from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import {
+  LoadingComponent,
   PjUiRouterNavigationElement,
   RouterNavComponent,
 } from '@peterjokumsen/ui-elements';
 
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { HttpCallCountService } from '@peterjokumsen/ng-services';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -16,14 +26,17 @@ import { faCode } from '@fortawesome/free-solid-svg-icons';
     NgOptimizedImage,
     RouterNavComponent,
     FaIconComponent,
+    LoadingComponent,
   ],
   templateUrl: './header.component.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
+  private _httpCount = inject(HttpCallCountService);
   imageSrc = 'assets/logo-150.webp';
 
+  loadingCount = toSignal(this._httpCount.count$.pipe(debounceTime(10)));
   codeIcon = faCode;
   navElements = input<PjUiRouterNavigationElement[]>([]);
 }
