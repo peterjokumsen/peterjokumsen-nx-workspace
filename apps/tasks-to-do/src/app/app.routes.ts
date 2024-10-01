4811c87468d8950378b5214067490d3337063341
Peter Jokumsen <pjokumsen@gmail.com>
2024-10-01 22:43:55 +0200

    chore(ttd): apply formatting

--- apps/tasks-to-do/src/app/app.routes.ts
+++ apps/tasks-to-do/src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
   {
     path: '',
-    loadComponent: () => import('./pages/primary-landing.component').then(m => m.PrimaryLandingComponent),
-  }
+    loadComponent: () =>
+      import('./pages/primary-landing.component').then(
+        (m) => m.PrimaryLandingComponent,
+      ),
+  },
 ];
