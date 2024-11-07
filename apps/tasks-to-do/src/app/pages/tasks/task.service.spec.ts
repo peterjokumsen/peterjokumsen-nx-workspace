8dcbc9bf9837f88925ec936af5b18ac9c07431e3
Peter Jokumsen <pjokumsen@gmail.com>
2024-11-07 21:34:18 +0200

    feat(ttd): display tasks

---
+++ apps/tasks-to-do/src/app/pages/tasks/task.service.spec.ts
@@ -0,0 +1,17 @@
+import { TaskService } from './task.service';
+import { TestBed } from '@angular/core/testing';
+
+describe('TaskService', () => {
+  let service: TaskService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [TaskService],
+    });
+    service = TestBed.inject(TaskService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
