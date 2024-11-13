b4dbd90359d894c2ed005082ed9e03c5419c238e
Peter Jokumsen <pjokumsen@gmail.com>
2024-11-13 21:32:53 +0200

    fix(task-management): resolve test failure for tasks-display

--- ng-libs/task-management/src/lib/pages/tasks-display.component.spec.ts
+++ ng-libs/task-management/src/lib/pages/tasks-display.component.spec.ts
@@ -1,14 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { RouterModule } from '@angular/router';
+import { TasksDataService } from '../services';
 import { TasksDisplayComponent } from './tasks-display.component';
+import { of } from 'rxjs';
 
 describe('TasksDisplayComponent', () => {
   let component: TasksDisplayComponent;
   let fixture: ComponentFixture<TasksDisplayComponent>;
 
+  let tasksDataMock: jest.Mocked<TasksDataService>;
+
   beforeEach(async () => {
+    tasksDataMock = {
+      getTasks: jest.fn(() => of([])).mockName('getTasks'),
+    } as unknown as jest.Mocked<TasksDataService>;
+
     await TestBed.configureTestingModule({
-      imports: [TasksDisplayComponent],
+      providers: [
+        {
+          provide: TasksDataService,
+          useValue: tasksDataMock,
+        },
+      ],
+      imports: [
+        // keep split
+        RouterModule.forRoot([]),
+        TasksDisplayComponent,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TasksDisplayComponent);
