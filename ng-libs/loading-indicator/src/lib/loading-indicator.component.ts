0a3357eec492362e698fd890cd3bad42344de07e
Peter Jokumsen <pjokumsen@gmail.com>
2024-11-14 23:18:03 +0200

    feat(loading-indicator): add progress spinner to component

--- ng-libs/loading-indicator/src/lib/loading-indicator.component.ts
+++ ng-libs/loading-indicator/src/lib/loading-indicator.component.ts
@@ -1,13 +1,41 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
 
 import { CommonModule } from '@angular/common';
+import { LoadingService } from './loading.service';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'pj-loading-indicator',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>loading-indicator works!</p>`,
+  imports: [CommonModule, MatProgressSpinner],
+  template: `
+    <div class="loading-indicator">
+      <mat-progress-spinner mode="indeterminate"></mat-progress-spinner>
+    </div>
+  `,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[style.display]': 'service?.isLoading() ? "block" : "none"',
+  },
 })
-export class LoadingIndicatorComponent {}
+export class LoadingIndicatorComponent implements OnInit {
+  service = inject(LoadingService, { optional: true });
+
+  ngOnInit() {
+    if (!this.service) {
+      throw new Error(
+        [
+          'LoadingIndicatorComponent (pj-loading-indicator)',
+          'requires LoadingService to be provided,',
+          'use provideLoadingIndicator() from @peterjokumsen/loading-indicator',
+        ].join(' '),
+      );
+    }
+  }
+}
