bc2e44a9221e66ce78a2522b49f6f80cdb54341b
Peter Jokumsen <pjokumsen@gmail.com>
2024-11-15 00:15:30 +0200

    feat(task-management): use loading-indicator

--- ng-libs/task-management/src/lib/lib.routes.ts
+++ ng-libs/task-management/src/lib/lib.routes.ts
@@ -1,10 +1,12 @@
 import { Route } from '@angular/router';
 import { TaskManagementComponent } from './task-management/task-management.component';
+import { provideLoadingIndicator } from '@peterjokumsen/loading-indicator';
 
 export const taskManagementRoutes: Route[] = [
   {
     path: '',
     component: TaskManagementComponent,
+    providers: [provideLoadingIndicator()],
     children: [
       {
         path: '',
--- ng-libs/task-management/src/lib/task-management/task-management.component.ts
+++ ng-libs/task-management/src/lib/task-management/task-management.component.ts
@@ -2,12 +2,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 import { CommonModule } from '@angular/common';
+import { LoadingIndicatorComponent } from '@peterjokumsen/loading-indicator';
 import { TasksDataService } from '../services';
 
 @Component({
   selector: 'task-mgr-task-management',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet],
+  imports: [CommonModule, RouterLink, RouterOutlet, LoadingIndicatorComponent],
   providers: [TasksDataService],
   templateUrl: './task-management.component.html',
   styleUrl: './task-management.component.scss',
