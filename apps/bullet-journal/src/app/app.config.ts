64b36d4d74f9bd6d40ec8f1a82989b2723ba9773
Peter Jokumsen <pjokumsen@gmail.com>
2024-12-30 22:29:29 +0200

    fix typo in folder name

--- apps/bullet-journal/src/app/app.config.ts
+++ apps/bullet-journal/src/app/app.config.ts
@@ -3,11 +3,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes), provideAnimationsAsync(), provideAnimationsAsync(),
   ],
 };
