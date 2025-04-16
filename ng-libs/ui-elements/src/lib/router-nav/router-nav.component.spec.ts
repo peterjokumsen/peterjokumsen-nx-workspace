10e5e7840e005a22cfbb50b6409abf8895c411b9
Peter Jokumsen <pjokumsen@gmail.com>
2025-04-17 01:35:39 +0200

    test(router-nav): simplify test setup by removing unnecessary component overrides and adding required Angular Material modules

--- ng-libs/ui-elements/src/lib/router-nav/router-nav.component.spec.ts
+++ ng-libs/ui-elements/src/lib/router-nav/router-nav.component.spec.ts
@@ -8,6 +8,9 @@ import {
 } from './models';
 
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterModule } from '@angular/router';
 import { RouterNavComponent } from './router-nav.component';
 
 @Component({
@@ -38,13 +41,7 @@ describe('RouterNavComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterNavHostComponent],
-    })
-      .overrideComponent(RouterNavComponent, {
-        set: {
-          imports: [CommonModule],
-        },
-      })
-      .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RouterNavHostComponent);
     hostComponent = fixture.componentInstance;
