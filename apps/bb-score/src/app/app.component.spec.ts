8e56f588d7a8da5369be48596388d82d4a2a4988
Peter Jokumsen <pjokumsen@gmail.com>
2025-04-18 22:23:29 +0200

    feat: enhance bb-score app layout and functionality
    Updated the layout of the BB Score application by replacing the header with a responsive sidenav component. Added navigation routes and a theme selection menu. Enhanced the app component to handle mobile responsiveness and navigation interactions. Updated tests to include the new BreakpointObserver dependency.

--- apps/bb-score/src/app/app.component.spec.ts
+++ apps/bb-score/src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter, RouterOutlet } from '@angular/router';
+import { of } from 'rxjs';
 import { AppComponent } from './app.component';
 import { ThemeService } from './core/services/theme.service';
 
@@ -8,8 +10,12 @@ describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
 
   let themeService: Pick<jest.Mocked<ThemeService>, 'setTheme'>;
+  let breakpointObserver: Pick<jest.Mocked<BreakpointObserver>, 'observe'>;
 
   beforeEach(async () => {
+    breakpointObserver = {
+      observe: jest.fn().mockReturnValue(of({ matches: false })),
+    };
     themeService = {
       setTheme: jest.fn().mockName('setTheme'),
     };
@@ -18,6 +24,7 @@ describe('AppComponent', () => {
       imports: [AppComponent, RouterOutlet],
       providers: [
         { provide: ThemeService, useValue: themeService },
+        { provide: BreakpointObserver, useValue: breakpointObserver },
         provideRouter([]),
       ],
     }).compileComponents();
