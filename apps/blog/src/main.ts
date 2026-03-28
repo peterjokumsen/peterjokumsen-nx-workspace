a40c34382a7864f309624bc14416b1e1c7354ddf
Peter Jokumsen <pjokumsen@gmail.com>
2026-03-28 13:51:19 +0200

    ran migrations for nx migrate

--- apps/blog/src/main.ts
+++ apps/blog/src/main.ts
@@ -1,7 +1,8 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { SplashComponent } from './app/splash.component';
 
-bootstrapApplication(SplashComponent, appConfig).catch((err) =>
+bootstrapApplication(SplashComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers]}).catch((err) =>
   console.error(err),
 );
